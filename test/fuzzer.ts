0370039de2e4a1384c15a4635b41fbbe34e96918
Seph Gentle <me@josephg.com>
2023-11-04 10:20:14 +1100

    Updated package.json to build and run tests, and export everything correctly.

--- test/fuzzer.ts
+++ test/fuzzer.ts
@@ -128,7 +128,7 @@ function fuzzer(seed: number) {
 }
 
 function fuzzLots() {
-  for (let i = 0; i < 100000; i++) {
+  for (let i = 0; i < 100; i++) {
     if (i % 10 === 0) console.log('i', i)
     try {
       fuzzer(i)
